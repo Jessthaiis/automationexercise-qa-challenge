7075fe47e9c09e69b70ce269391cd382da4669a3
Jessthaiis <jessica_thaismachado@hotmail.com>
2026-05-14 23:39:42 -0300

    chore: ajustar viewport para 1280x720

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationexercise.com',
+    viewportWidth: 1280,  
+    viewportHeight: 720,  
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
